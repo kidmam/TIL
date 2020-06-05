254afaa37aa8b58988c5e466d872325a1e7b2ddf
SangKil Park <ht1911@gmail.com>
2020-06-06 06:57:59 +0900

    Simple, performant, goroutine safe queues, useful as resource pools or job queues.

---
+++ datastructures/blockingqueues/blockingqueues/abstractDataTypes.go
@@ -0,0 +1,29 @@
+package blockingqueues
+
+type AbstractCollectionBase interface {
+	Size() uint64
+	Capacity() uint64
+	IsEmpty() bool
+	Clear()
+}
+
+// All Queues must implement this interface
+type Interface interface {
+	AbstractCollectionBase
+
+	Push(item interface{}) (bool, error)
+	Pop() (interface{}, error)
+
+	Get() (interface{}, error)
+	Put(item interface{}) (bool, error)
+	Offer(item interface{}) bool
+
+	Peek() interface{}
+}
+
+type QueueStore interface {
+	Set(value interface{}, pos uint64)
+	Remove(pos uint64) interface{}
+	Get(pos uint64) interface{}
+	Size() uint64
+}
