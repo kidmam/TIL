f20309f368258f3d802b036a8b948b6c8b550008
SangKil Park <ht1911@gmail.com>
2020-06-10 07:26:54 +0900

    Quick Sort

---
+++ datastructures/quicksort/quicksort.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	intArray := []int{0, 2, 3, 4, 11, 34}
+	searchNumber := 34
+
+	sorted := sort.SearchInts(intArray, searchNumber)
+
+	if sorted < len(intArray) {
+		fmt.Printf("Found element %d at array position %d \n", searchNumber, sorted)
+	} else {
+		fmt.Printf("Element %d not found in array %v \n", searchNumber, intArray)
+	}
+}
