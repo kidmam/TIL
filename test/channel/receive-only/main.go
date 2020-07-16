acbf1635732b9d4a58f6022c808e154262ea1db7
SangKil Park <ht1911@gmail.com>
2020-07-16 19:07:40 +0900

    Use Channels as Futures/Promises.

---
+++ test/channel/receive-only/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func longTimeRequest() <-chan int32 {
+	r := make(chan int32)
+
+	go func() {
+		// Simulate a workload.
+		time.Sleep(time.Second * 3)
+		r <- rand.Int31n(100)
+	}()
+
+	return r
+}
+
+func sumSquares(a, b int32) int32 {
+	return a*a + b*b
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	a, b := longTimeRequest(), longTimeRequest()
+	fmt.Println(sumSquares(<-a, <-b))
+}
