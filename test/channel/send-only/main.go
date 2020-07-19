671951481c409947bf505d1b5778c8b300b14c74
SangKil Park <ht1911@gmail.com>
2020-07-19 16:56:27 +0900

    Pass send-only channels as arguments.

--- test/channel/send-only/main.go
+++ test/channel/send-only/main.go
@@ -19,9 +19,15 @@ func sumSquares(a, b int32) int32 {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	ra, rb := make(chan int32), make(chan int32)
+	/*ra, rb := make(chan int32), make(chan int32)
 	go longTimeRequest(ra)
 	go longTimeRequest(rb)
+	fmt.Println(sumSquares(<-ra, <-rb)) */
 
-	fmt.Println(sumSquares(<-ra, <-rb))
+	results := make(chan int32, 2)
+	go longTimeRequest(results)
+	go longTimeRequest(results)
+
+	fmt.Println(sumSquares(<-results, <-results))
+
 }
