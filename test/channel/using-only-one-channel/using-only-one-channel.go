e0a0c3268f445f6e30b69f25aa1c600d25bf72dc
SangKil Park <ht1911@gmail.com>
2020-07-21 06:33:44 +0900

    The first response wins

---
+++ test/channel/using-only-one-channel/using-only-one-channel.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func source(c chan<- int32) {
+	ra, rb := rand.Int31(), rand.Intn(3)+1
+	// Sleep 1s/2s/3s.
+	time.Sleep(time.Duration(rb) * time.Second)
+	c <- ra
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	startTime := time.Now()
+	// c must be a buffered channel.
+	c := make(chan int32, 5)
+	for i := 0; i < cap(c); i++ {
+		go source(c)
+	}
+	// Only the first response will be used.
+	rnd := <-c
+	fmt.Println(time.Since(startTime))
+	fmt.Println(rnd)
+}
