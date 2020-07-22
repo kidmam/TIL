3b9c37cd837db8a889ec03d6d7d2832ff4774c96
SangKil Park <ht1911@gmail.com>
2020-07-23 06:35:45 +0900

    How to: Global Constant Maps, Slices, & Arrays in Golang

---
+++ test/globals-and-constants/main.go
@@ -0,0 +1,9 @@
+package globals_and_constants
+
+// https://qvault.io/2019/10/21/golang-constant-maps-slices/
+
+// this is a global constant
+const safeRateLimit = 10
+
+// this is a global variable
+var dangerousRateLimit = 10
