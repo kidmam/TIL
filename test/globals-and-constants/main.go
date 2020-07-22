047ef9839e8c841f8a106856611cbc88e84bbabf
SangKil Park <ht1911@gmail.com>
2020-07-23 07:22:07 +0900

    How to: Global Constant Maps, Slices, & Arrays in Golang

--- test/globals-and-constants/main.go
+++ test/globals-and-constants/main.go
@@ -7,3 +7,20 @@ const safeRateLimit = 10
 
 // this is a global variable
 var dangerousRateLimit = 10
+
+const rateLimit = 10
+
+// this is meant to be constant! Please don't mutate it!
+var supportedNetworks = []string{"facebook", "twitter", "instagram"}
+
+func getSupportedNetworks() []string {
+	return []string{"facebook", "twitter", "instagram"}
+}
+
+func getSupportedNetworkRatelimits() map[string]int {
+	return map[string]int{
+		"facebook":  10,
+		"twitter":   50,
+		"instagram": 60,
+	}
+}
