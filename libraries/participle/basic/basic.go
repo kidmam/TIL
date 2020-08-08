bac494bdc2eac56605d4f635b1a502b3e6ddc092
SangKil Park <ht1911@gmail.com>
2020-08-08 15:23:25 +0900

    A parser library for Go.

---
+++ libraries/participle/basic/basic.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/alecthomas/repr"
+
+	"github.com/alecthomas/participle"
+	"github.com/alecthomas/participle/lexer"
+	"github.com/alecthomas/participle/lexer/ebnf"
+)
+
+type File struct {
+	Entries []*Entry `@@*`
+}
+
+type Entry struct {
+	Type   *Type   `  @@`
+	Schema *Schema `| @@`
+	Enum   *Enum   `| @@`
+	Scalar string  `| "scalar" @Ident`
+}
+
+type Enum struct {
+	Name  string   `"enum" @Ident`
+	Cases []string `"{" @Ident* "}"`
+}
+
+type Schema struct {
+	Fields []*Field `"schema" "{" @@* "}"`
+}
+
+type Type struct {
+	Name       string   `"type" @Ident`
+	Implements string   `("implements" @Ident)?`
+	Fields     []*Field `"{" @@* "}"`
+}
+
+type Field struct {
+	Name       string      `@Ident`
+	Arguments  []*Argument `("(" (@@ ("," @@)*)? ")")?`
+	Type       *TypeRef    `":" @@`
+	Annotation string      `("@" @Ident)?`
+}
+
+type Argument struct {
+	Name    string   `@Ident`
+	Type    *TypeRef `":" @@`
+	Default *Value   `("=" @@)?`
+}
+
+type TypeRef struct {
+	Array       *TypeRef `(   "[" @@ "]"`
+	Type        string   `  | @Ident )`
+	NonNullable bool     `@"!"?`
+}
+
+type Value struct {
+	Symbol string `@Ident`
+}
+
+var (
+	graphQLLexer = lexer.Must(ebnf.New(`
+    Comment = ("#" | "//") { "\u0000"…"\uffff"-"\n" } .
+    Ident = (alpha | "_") { "_" | alpha | digit } .
+    Number = ("." | digit) {"." | digit} .
+    Whitespace = " " | "\t" | "\n" | "\r" .
+    Punct = "!"…"/" | ":"…"@" | "["…` + "\"`\"" + ` | "{"…"~" .
+
+    alpha = "a"…"z" | "A"…"Z" .
+    digit = "0"…"9" .
+`))
+
+	parser = participle.MustBuild(&File{},
+		participle.Lexer(graphQLLexer),
+		participle.Elide("Comment", "Whitespace"),
+	)
+
+	cli struct {
+		Files []string `arg:"" type:"existingfile" required:"" help:"GraphQL schema files to parse."`
+	}
+)
+
+func main() {
+	ctx := kong.Parse(&cli)
+	for _, file := range cli.Files {
+		ast := &File{}
+		r, err := os.Open(file)
+		ctx.FatalIfErrorf(err)
+		err = parser.Parse(r, ast)
+		r.Close()
+		repr.Println(ast)
+		ctx.FatalIfErrorf(err)
+	}
+}
