7e0f4e58721a6cf7554e14026d6a80d3e6784ebe
SangKil Park <ht1911@gmail.com>
2020-08-23 13:34:13 +0900

    Create Go contexts that cancel on signals.

---
+++ test/go-signalcontext/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/sethvargo/go-signalcontext"
+)
+
+func main() {
+	ctx, cancel := signalcontext.OnInterrupt()
+	defer cancel()
+
+	s := &http.Server{
+		Addr: ":8080",
+	}
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for CTRL+C
+	<-ctx.Done()
+
+	// Stop the server
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(shutdownCtx); err != nil {
+		log.Fatal(err)
+	}
+}
